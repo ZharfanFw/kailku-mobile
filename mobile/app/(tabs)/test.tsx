c7eacd112e34ece4842342e3a66107a8f63c6aa1
ZharfanFw <zharfanfaz21@gmail.com>
2025-12-09 21:41:30 +0700

    Initial commit from Create Expo App and Fastify for Backend

---
+++ mobile/app/(tabs)/test.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native';
+
+export default function Home() {
+  return (
+    <View className="flex-1 bg-red-500 items-center justify-center">
+      <Text className="text-white font-bold text-3xl">Tes NativeWind</Text>
+    </View>
+  );
+}
